eb8a2d817d4a97c30ac5a033b6cf1dd9cae04005
Goli <100245222+ManuelP2002@users.noreply.github.com>
2024-09-26 10:51:20 -0500

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Goli.js
@@ -0,0 +1,42 @@
+// 1. Create a comment and then add the url from the official website of your language of choice:
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript
+
+// 2. Represent the different ways to write a comment
+
+// A single-line comment
+
+/* A second single-line comment*/
+
+/* 
+A comment 
+which stretches 
+over several 
+lines
+*/
+
+// Note: comments prevents code execution
+
+// 3. Initialize a variable and a const (if supported)
+
+var x;
+const y = 0;
+
+// 4. Initialize multiple variables to represent all primitive types supported
+/* In Javascript, a primitive is a data type 
+that is not an object, the is 7 in total: */
+
+var string = "Hello, world!"; // String, sequence of characters
+var integer = 1; // Number
+var decimal = 1.1; // Number
+var bigInt = 2 ** 53; // BigInt
+var boolTrue = true; // Boolean, logical data type, only true or false
+var boolFalse = false; // Boolean
+var undefined; // Undefined, value asigned to newly declared variables
+var symbol = null; // Symbol [Null], represents empty or invalid objects
+
+// Note: when using the typeof operator null returns "object";
+
+// 5. Print "Hello, [language name]!""
+
+console.log("Hello, JavaScript!");
